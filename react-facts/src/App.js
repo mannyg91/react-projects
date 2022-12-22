a391b3753d44c0faadb51bc742c9a0dd644fcfdb
Manny Gomez <mjgomez@nevada.unr.edu>
2022-12-22 13:11:48 -0800

    updated project name

---
+++ react-facts/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Main from './components/Main.js';
+import Navbar from './components/Navbar.js';
+
+function App() {
+    return (<div className="container">
+            <Navbar />
+            <Main />
+        </div>)
+}
+
+export default App;
