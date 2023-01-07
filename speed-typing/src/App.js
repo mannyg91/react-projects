b4271bb4fe7ce84b300f9dd32f1bb37bd1087031
Manny Gomez <mjgomez@nevada.unr.edu>
2023-01-07 09:14:46 -0800

    added wordCount and handleChange functions

--- speed-typing/src/App.js
+++ speed-typing/src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
 
 function App() {
-  return (
-    <main>    
-      <h1>How fast do you type?</h1>
-      <textarea />
-      <h4>Time remaining: ???</h4>
-      <button>Start</button>
-      <h1>Word count: ???</h1>
-    </main>
 
-  );
+
+
+    const [text, setText] = React.useState("")
+    
+    function handleChange(e) {
+        const {value} = e.target
+        setText(value)
+        console.log(text)
+    }
+
+    function calculateWordCount(text) {
+        const wordsArr = text.split(' ')
+        const wordCount = wordsArr.length
+        console.log(wordCount)
+        return wordCount
+    }
+
+
+    return (
+        <main>
+            <h1>How fast do you type?</h1>
+            <textarea
+                onChange={handleChange}
+                value={text} // not completely necessary, but ensures there is one source of truth
+            />
+            <h4>Time remaining: ???</h4>
+            <button onClick={() => calculateWordCount(text)}>Start</button>
+            <h1>Word count: ???</h1>
+        </main>
+
+    );
 }
 
 export default App;
