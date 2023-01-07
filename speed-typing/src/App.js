93a54cfbbe849cabb1ad25b1b88c0f4369c36dd6
Manny Gomez <mjgomez@nevada.unr.edu>
2023-01-07 11:19:13 -0800

    added useRef to focus text area

--- speed-typing/src/App.js
+++ speed-typing/src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 function App() {
+    const STARTING_TIME = 5
 
-    const [text, setText] = React.useState("")
-    const [timeRemaining, setTimeRemaining] = React.useState(5)
-    const [isTimeRunning, setIsTimeRunning] = React.useState(false)
-    const [wordCount, setWordCount] = React.useState(0)
+    const [text, setText] = useState("")
+    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
+    const [wordCount, setWordCount] = useState(0)
+    const textAreaRef = useRef(null)
 
-    React.useEffect(() => {
+    useEffect(() => {
         if (isTimeRunning && timeRemaining > 0) {        
             setTimeout(() => {
                 setTimeRemaining(time => time - 1)
             }, 1000)
         } else if (timeRemaining === 0) {
-            setIsTimeRunning(false)
-            setWordCount(calculateWordCount(text))
+            endGame()
         }
     }, [timeRemaining, isTimeRunning])
 
     // const wordCount = timeRemaining === 0 ? calculateWordCount(text) : 0;
+    function startGame() {
+        setIsTimeRunning(true)
+        setTimeRemaining(STARTING_TIME)
+        setText("")
+        console.log(textAreaRef)
+        textAreaRef.current.disabled = false
+        textAreaRef.current.focus()
+    }
+
+    function endGame() {
+        setIsTimeRunning(false)
+        setWordCount(calculateWordCount(text))
+    }
+
+
 
     function handleChange(e) {
         const {value} = e.target
@@ -38,11 +54,13 @@ function App() {
         <main>
             <h1>How fast do you type?</h1>
             <textarea
+                ref={textAreaRef}
+                disabled={!isTimeRunning}
                 onChange={handleChange}
                 value={text} // not completely necessary, but ensures there is one source of truth
             />
             <h4>Time remaining: {timeRemaining}</h4>
-            <button onClick={() => setIsTimeRunning(true)}>Start</button>
+            <button disabled={isTimeRunning} onClick={startGame}>Start</button>
             <h1>Word count: {wordCount}</h1>
         </main>
 
