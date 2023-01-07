ec074c67224d8f7d6a8208f93e147dff7647e4ce
Manny Gomez <mjgomez@nevada.unr.edu>
2023-01-07 09:37:37 -0800

    added timeRemaining state and useEffect

--- speed-typing/src/App.js
+++ speed-typing/src/App.js
@@ -5,6 +5,16 @@ function App() {
 
 
     const [text, setText] = React.useState("")
+    const [timeRemaining, setTimeRemaining] = React.useState(5)
+
+    React.useEffect(() => {
+        if (timeRemaining > 0) {        
+            setTimeout(() => {
+                setTimeRemaining(time => time - 1)
+            }, 1000)
+        }
+    }, [timeRemaining])
+
     
     function handleChange(e) {
         const {value} = e.target
@@ -13,8 +23,9 @@ function App() {
     }
 
     function calculateWordCount(text) {
-        const wordsArr = text.split(' ')
-        const wordCount = wordsArr.length
+        const wordsArr = text.trim().split(' ')
+        const filteredWords = wordsArr.filter(word => word !== "") // solves problem of blank textarea being counted as 1 word
+        const wordCount = filteredWords.length
         console.log(wordCount)
         return wordCount
     }
@@ -27,7 +38,7 @@ function App() {
                 onChange={handleChange}
                 value={text} // not completely necessary, but ensures there is one source of truth
             />
-            <h4>Time remaining: ???</h4>
+            <h4>Time remaining: {timeRemaining}</h4>
             <button onClick={() => calculateWordCount(text)}>Start</button>
             <h1>Word count: ???</h1>
         </main>
