31dbb85a2d6b2d385b0f24b2c8f028bc62ec234e
Manny Gomez <mjgomez@nevada.unr.edu>
2023-01-07 08:52:45 -0800

    added new speed-typing project, and minor fixes with basic-template

--- basic-template/src/App.js
+++ basic-template/src/App.js
@@ -1,8 +1,8 @@
-import Navbar from './components/Navbar';
+import React from 'react';
 
 function App() {
   return (
-    <Navbar />
+    <h1>Hello World</h1>
   );
 }
 
---
+++ speed-typing/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main>    
+      <h1>How fast do you type?</h1>
+      <textarea />
+      <h4>Time remaining: ???</h4>
+      <button>Start</button>
+      <h1>Word count: ???</h1>
+    </main>
+
+  );
+}
+
+export default App;
