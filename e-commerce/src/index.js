17030b2c02d103b2fa05cf2632f31d94933dd191
Manny Gomez <mjgomez@nevada.unr.edu>
2023-01-09 09:50:34 -0800

    setup context provider

--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from "react-router-dom"
 
 import App from './App';
+import {ContextProvider} from "./context"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
-        <App />
-    </Router>
+    <ContextProvider>
+        <Router>
+            <App />
+        </Router>
+    </ContextProvider>
+
 );
 
