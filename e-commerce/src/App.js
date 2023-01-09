7a1f3048b578ac384134a4fab672eb078a045269
Manny Gomez <mjgomez@nevada.unr.edu>
2023-01-09 09:42:38 -0800

    added initial routes

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -3,12 +3,25 @@ import Header from "./components/Header"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
 
+import { Route, Routes } from 'react-router-dom'
+
+
+
 function App() {    
     return (
         <div>
             <Header />
             <h1>Home Page</h1>
+         
+            <Routes>
+              <Route exact path="/" element={<Photos />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+            
+
         </div>
+
+
     )
 }
 
