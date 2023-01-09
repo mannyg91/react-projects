83756dd3789a0a7660a088ba2a997a842ac3a096
Manny Gomez <mjgomez@nevada.unr.edu>
2023-01-09 09:53:54 -0800

    setup state for photos

--- e-commerce/src/context.js
+++ e-commerce/src/context.js
@@ -1,10 +1,13 @@
-import React from "react"
+import React, {useState} from "react"
 
 const Context = React.createContext() 
 
 function ContextProvider(props) {
+    const [photos, setPhotos] = useState([])
+
+
     return (
-        <Context.Provider value={""}>
+        <Context.Provider value={photos}>
             {props.children}
         </Context.Provider>
     )
