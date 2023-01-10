203d47ddba26cd071e1a1e65e9736e09ade39b93
Manny Gomez <mjgomez@nevada.unr.edu>
2023-01-10 12:18:54 -0800

    Added total cost function

--- e-commerce/src/pages/Cart.js
+++ e-commerce/src/pages/Cart.js
@@ -3,16 +3,23 @@ import {Context} from "../Context"
 import CartItem from "../components/CartItem"
 
 function Cart() {
+
     const {cartItems} = useContext(Context)
+
     const cartItemElements = cartItems.map(item => (
         <CartItem key={item.id} item={item} />
     ))
+
+    function totalCost() {
+        const totalCost = 5.99 * cartItems.length
+        return totalCost.toLocaleString("en-US", {style: "currency", currency: "USD"})
+    }
 
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItemElements}
-            <p className="total-cost">Total: </p>
+            <p className="total-cost">Total: {totalCost()} </p>
             <div className="order-button">
                 <button>Place Order</button>
             </div>
